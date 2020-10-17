47e9356eba86ff02d5e1a9bac794f2d4484bd68a
jhonnyenglish3703 <73019250+jhonnyenglish3703@users.noreply.github.com>
2020-10-17 15:38:20 +0530

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,30 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
-import 'package:http/http.dart' as http;
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  List currencies;
-  final List<MaterialColor> colors = [
-    Colors.brown,
-    Colors.blue,
-    Colors.green,
-    Colors.red
-  ];
-
-  @override
-  void initState() async{
-    super.initState;
-    currencies = await getCurrencies();
-  }
-
-  Future<List> getCurrencies() async {
-    String cryptourl = "https://api.coinmarketcap.com/v1/ticker/?limit=50";
+imporz=x+yurl = "https://api.coinmarketcap.com/v1/ticker/?limit=50";
     http.Response response = await http.get(cryptourl);
     return json.decode(response.body);
   }
@@ -40,7 +17,21 @@ class _HomePageState extends State<HomePage> {
   }
 
 
-  Widget _cryptowidget() {
+  
+ngeWidget;
+    if(double.parse(percentage)>0){
+      percentageChangeWidget = TextSpan(
+        text: percentageChange,
+        style: TextStyle(color: Colors.green)
+      );
+    }
+    else{
+      percentageChangeWidget = TextSpan(
+          text: percentageChange,
+          style: TextStyle(color: Colors.red));
+  }
+}}
+Widget _cryptowidget() {
     return Container(
       child: Flexible(
           child: ListView.builder(
@@ -54,33 +45,3 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-
-  Widget getListItemUI(Map currency,MaterialColor color){
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundColor: color,
-        child: Text(currency['name'][0]),
-      ),
-      title: Text(currency['name'],
-       style: TextStyle(fontWeight: FontWeight.bold),),
-      subtitle: getSubtitleText(currency['price_usd'],currency['percentage_change_1h'])
-    );
-  }
-  Widget getSubtitleText(String price,String percentage){
-    TextSpan priceText = TextSpan(
-      text: "\$$price"
-    );
-    String percentageChange = "1 Hour: $percentage%";
-    TextSpan percentageChangeWidget;
-    if(double.parse(percentage)>0){
-      percentageChangeWidget = TextSpan(
-        text: percentageChange,
-        style: TextStyle(color: Colors.green)
-      );
-    }
-    else{
-      percentageChangeWidget = TextSpan(
-          text: percentageChange,
-          style: TextStyle(color: Colors.red));
-  }
-}}
